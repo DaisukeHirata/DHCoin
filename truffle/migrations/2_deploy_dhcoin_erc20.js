23ee3592831253e346335c8024a27f597ac161d5
Daisuke Hirata <daisukihirata@gmail.com>
2018-06-12 21:30:30 +0900

    fix typo

--- truffle/migrations/2_deploy_dhcoin_erc20.js
+++ truffle/migrations/2_deploy_dhcoin_erc20.js
@@ -1,9 +1,9 @@
-const InnoCoin = artifacts.require('./DHCoin.sol')
+const DHCoin = artifacts.require('./DHCoin.sol')
 
 module.exports = (deployer) => {
     let initialSupply = 1000000e18
     let maxSupply = 100000000e18
-    deployer.deploy(InnoCoin, initialSupply, maxSupply, {
+    deployer.deploy(DHCoin, initialSupply, maxSupply, {
         gas: 2000000
     })
 }
